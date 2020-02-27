85764dde26ca28f5755b5249d81f183f0c58a444
checkroth <c.heckroth@joyz.co.jp>
2020-02-27 09:53:15 +0900

    add safe switch for fifo vs normal

--- pysqs_extended_client/SQSClientExtended.py
+++ pysqs_extended_client/SQSClientExtended.py
@@ -1,13 +1,13 @@
-import os
-import json
-import uuid
 import base64
-import boto3
+import json
+import os
 import tempfile
-
+import uuid
 from enum import Enum
-from boto3.session import Session
 from io import BytesIO
+
+import boto3
+from boto3.session import Session
 
 
 class SQSExtendedClientConstants(Enum):
@@ -36,7 +36,8 @@ class SQSClientExtended(object):
 	:param profile_name: The name of a profile to use. If not given, then the default profile is used.
 	"""
 
-	def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, aws_region_name=None, s3_bucket_name=None, delete_from_s3=False):
+	def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, 
+	aws_region_name=None, s3_bucket_name=None, delete_from_s3=False):
 		self.aws_access_key_id = aws_access_key_id
 		self.aws_secret_access_key = aws_secret_access_key
 		self.aws_region_name = aws_region_name
@@ -83,7 +84,7 @@ class SQSClientExtended(object):
 		try:
 			encoded = self.__stringToBase64(self.__base64ToString(s))
 			return encoded == str.encode(s)
-		except Exception as e:
+		except Exception:
 			return False
 
 	def __get_msg_attributes_size(self, message_attributes):
@@ -121,20 +122,23 @@ class SQSClientExtended(object):
 			An MD5 digest of the message attributes.
 			The receipt handle is the identifier you must provide when deleting the message
 		"""
-		response_opt_queue = self.sqs.receive_message(QueueUrl=queue_url, AttributeNames=['All'], MessageAttributeNames=[
-		                                              'All', ], MaxNumberOfMessages=max_number_Of_Messages, WaitTimeSeconds=wait_time_seconds,)
+		response_opt_queue = self.sqs.receive_message(
+			QueueUrl=queue_url, AttributeNames=['All'], MessageAttributeNames=['All',], 
+			MaxNumberOfMessages=max_number_Of_Messages, WaitTimeSeconds=wait_time_seconds,)
 		opt_messages = response_opt_queue.get('Messages', [])
 		if not opt_messages:
 			return None
 		for message in opt_messages:
-			large_pay_load_attribute_value = message.get('MessageAttributes', {}).get(SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value, None)
+			large_pay_load_attribute_value = message.get('MessageAttributes', {}).get(
+				SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value, None)
 			if large_pay_load_attribute_value:
 				try:
 					message_body = json.loads(message.get('Body'))
 					if isinstance(message_body, list) and message_body[0] == "software.amazon.payloadoffloading.PayloadS3Pointer":
 						message_body = message_body[1]
 					if 's3BucketName' not in message_body and 's3Key' not in message_body:
-						raise ValueError('Detected missing required key attribute s3BucketName and s3Key in s3 payload')
+						raise ValueError(
+							'Detected missing required key attribute s3BucketName and s3Key in s3 payload')
 					s3_bucket_name = message_body.get('s3BucketName')
 					s3_key = message_body.get('s3Key')
 					orig_msg_body = self.get_text_from_s3(s3_bucket_name, s3_key)
@@ -142,8 +146,10 @@ class SQSClientExtended(object):
 					# remove the additional attribute before returning the message to user.
 					message.get('MessageAttributes').pop(SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value)
 					# Embed s3 object pointer in the receipt handle.
-					modified_receipt_handle = SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value + s3_bucket_name + SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value + \
-					    SQSExtendedClientConstants.S3_KEY_MARKER.value + s3_key + SQSExtendedClientConstants.S3_KEY_MARKER.value + message.get('ReceiptHandle')
+					modified_receipt_handle = SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value \
+						+ s3_bucket_name + SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value + \
+						SQSExtendedClientConstants.S3_KEY_MARKER.value + s3_key \
+						+ SQSExtendedClientConstants.S3_KEY_MARKER.value + message.get('ReceiptHandle')
 					message['ReceiptHandle'] = modified_receipt_handle
 				except ValueError:
 					raise ValueError('Decoding JSON has failed')
@@ -152,13 +158,17 @@ class SQSClientExtended(object):
 
 	def __delete_message_payload_from_s3(self, receipt_handle):
 		try:
-			s3_msg_bucket_name = self.__get_bucket_marker_from_receipt_handle(receipt_handle, SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value)
-			s3_msg_key = self.__get_bucket_marker_from_receipt_handle(receipt_handle, SQSExtendedClientConstants.S3_KEY_MARKER.value)
-			session = Session(aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key, region_name=self.aws_region_name)
+			s3_msg_bucket_name = self.__get_bucket_marker_from_receipt_handle(receipt_handle, 
+			SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value)
+			s3_msg_key = self.__get_bucket_marker_from_receipt_handle(receipt_handle, 
+			SQSExtendedClientConstants.S3_KEY_MARKER.value)
+			session = Session(aws_access_key_id=self.aws_access_key_id, 
+			aws_secret_access_key=self.aws_secret_access_key, region_name=self.aws_region_name)
 			s3 = session.resource('s3')
 			s3_object = s3.Object(s3_msg_bucket_name, s3_msg_key)
 			s3_object.delete()
-			print('Deleted s3 object https://s3.amazonaws.com/{}/{}'.format(s3_msg_bucket_name, s3_msg_key))
+			print('Deleted s3 object https://s3.amazonaws.com/{}/{}'.format(s3_msg_bucket_name, 
+			s3_msg_key))
 		except Exception as e:
 			print("Failed to delete the message content in S3 object. {}, type:{}".format(
 				str(e), type(e).__name__))
@@ -170,10 +180,13 @@ class SQSClientExtended(object):
 		return receipt_handle[start_marker:end_marker]
 
 	def __get_orig_receipt_handle(self, receipt_handle):
-		return receipt_handle[receipt_handle.rindex(SQSExtendedClientConstants.S3_KEY_MARKER.value) + len(SQSExtendedClientConstants.S3_KEY_MARKER.value):]
+		return receipt_handle[receipt_handle.rindex(
+			SQSExtendedClientConstants.S3_KEY_MARKER.value) + len(
+				SQSExtendedClientConstants.S3_KEY_MARKER.value):]
 
 	def __is_s3_receipt_handle(self, receipt_handle):
-		return True if SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value in receipt_handle and SQSExtendedClientConstants.S3_KEY_MARKER.value in receipt_handle else False
+		return True if SQSExtendedClientConstants.S3_BUCKET_NAME_MARKER.value in receipt_handle \
+			and SQSExtendedClientConstants.S3_KEY_MARKER.value in receipt_handle else False
 
 	def delete_message(self, queue_url, receipt_handle):
 		"""
@@ -193,6 +206,18 @@ class SQSClientExtended(object):
 		# print("receipt_handle={}".format(receipt_handle))
 		self.sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
+	def _send_queue_message(self, queue_url, message, message_group_id, 
+	message_deduplication_id, message_attributes, fifo_queue):
+		if fifo_queue:
+			return self.sqs.send_message(QueueUrl=queue_url, MessageBody=message, 
+			MessageGroupId=message_group_id, 
+			MessageDeduplicationId=message_deduplication_id, 
+			MessageAttributes=message_attributes)
+		else:
+			return self.sqs.send_message(QueueUrl=queue_url, 
+			MessageBody=message, 
+			MessageAttributes=message_attributes)
+
 	def send_message(self, queue_url, message, message_attributes=None, delay_seconds=0):
 		"""
 		Delivers a message to the specified queue and uploads the message payload
@@ -208,27 +233,36 @@ class SQSClientExtended(object):
 
     Using None is both safe and conventional in cases where arguments with default value are not passed.
 		"""
+		
+		msg_attributes_size = self.__get_msg_attributes_size(message_attributes)
+		if msg_attributes_size > self.message_size_threshold:
+			raise ValueError("Total size of Message attributes is {} bytes which is larger than the threshold of {} Bytes. Consider including the payload in the message body instead of message attributes.".format(msg_attributes_size, self.message_size_threshold))
 
 		if message_attributes is None:
 			message_attributes = dict()
-
-		message_attributes_number = len(message_attributes)
-		if message_attributes_number > SQSExtendedClientConstants.MAX_ALLOWED_ATTRIBUTES.value:
-			raise ValueError("Number of message attributes [{}}] exceeds the maximum allowed for large-payload messages [{}].".format(message_attributes_number, SQSExtendedClientConstants.MAX_ALLOWED_ATTRIBUTES.value))
 			
-		large_payload_attribute_value = message_attributes.get(SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value)
+		large_payload_attribute_value = message_attributes.get(
+			SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value)
 		if large_payload_attribute_value:
-			raise ValueError("Message attribute name {} is reserved for use by SQS extended client.".format(SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value))
+			raise ValueError(
+				"Message attribute name {} is reserved for use by SQS extended client.".format(
+					SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value))
 
 		if self.always_through_s3 or self.__is_large(str(message), message_attributes):
 			if not self.s3_bucket_name.strip():
 				raise ValueError('S3 bucket name cannot be null')
 			s3_key_message = json.dumps(self.__store_message_in_s3(message))
-			message_attributes[SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value] = {'StringValue': str(self.__get_string_size_in_bytes(message)), 'DataType': 'Number'}
-			return self.sqs.send_message(QueueUrl=queue_url, MessageBody=s3_key_message, MessageAttributes=message_attributes, DelaySeconds=delay_seconds)
+			message_attributes[SQSExtendedClientConstants.RESERVED_ATTRIBUTE_NAME.value] = {
+				'StringValue': str(self.__get_string_size_in_bytes(message)), 
+				'DataType': 'Number'}
+			return self.sqs.send_message(QueueUrl=queue_url, MessageBody=s3_key_message, 
+			MessageAttributes=message_attributes, DelaySeconds=delay_seconds)
+			# return self._send_queue_message(queue_url, s3_key_message, message_group_id, message_deduplication_id, message_attributes, fifo_queue)	
 		else:
-			return self.sqs.send_message(QueueUrl=queue_url, MessageBody=message, MessageAttributes=message_attributes, DelaySeconds=delay_seconds)
-
+			return self.sqs.send_message(QueueUrl=queue_url, MessageBody=message, 
+			MessageAttributes=message_attributes, DelaySeconds=delay_seconds)
+			# return self._send_queue_message(queue_url, s3_key_message, message_group_id, message_deduplication_id, message_attributes, fifo_queue)	
+	
 	def __store_message_in_s3(self, message_body):
 		"""
 		Store SQS message body into user defined s3 bucket
@@ -236,7 +270,8 @@ class SQSClientExtended(object):
 		"""
 		try:
 			s3_key = str(uuid.uuid4())
-			session = Session(aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key, region_name=self.aws_region_name)
+			session = Session(aws_access_key_id=self.aws_access_key_id, 
+			aws_secret_access_key=self.aws_secret_access_key, region_name=self.aws_region_name)
 			s3 = session.resource('s3')
 			opt_file = tempfile.NamedTemporaryFile(mode='w+', encoding='utf-8', delete=False)
 			opt_file.write(str(message_body))
@@ -249,7 +284,8 @@ class SQSClientExtended(object):
 				os.remove(opt_file.name)
 			return {'s3BucketName': self.s3_bucket_name, 's3Key': s3_key}
 		except Exception as e:
-			print("Failed to store the message content in an S3 object. SQS message was not sent. {}, type:{}".format(
+			print(
+				"Failed to store the message content in an S3 object. SQS message was not sent. {}, type:{}".format(
 				str(e), type(e).__name__))
 			raise e
 
@@ -257,7 +293,8 @@ class SQSClientExtended(object):
 		"""
 		Get string representation of a sqs object and store into original SQS message object
 		"""
-		session = Session(aws_access_key_id=self.aws_access_key_id, aws_secret_access_key=self.aws_secret_access_key, region_name=self.aws_region_name)
+		session = Session(aws_access_key_id=self.aws_access_key_id, 
+		aws_secret_access_key=self.aws_secret_access_key, region_name=self.aws_region_name)
 		s3 = session.resource('s3')
 		bucket = s3.Bucket(s3_bucket_name)
 		objs = list(bucket.objects.filter(Prefix=s3_key))
--- tests/test_pysqs_extended_client.py
+++ tests/test_pysqs_extended_client.py
@@ -1,12 +1,16 @@
-import pytest
 import base64
 
-from pysqs_extended_client.SQSClientExtended import SQSClientExtended
+import pytest
 
 # from string import ascii_letters, digits
 # from random import choice
-
-from pysqs_extended_client.config import (AWS_SQS_QUEUE_URL, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION)
+from pysqs_extended_client.config import (
+    AWS_ACCESS_KEY_ID,
+    AWS_DEFAULT_REGION,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_SQS_QUEUE_URL,
+)
+from pysqs_extended_client.SQSClientExtended import SQSClientExtended
 
 try:
     FileNotFoundError
@@ -14,13 +18,14 @@ except NameError:
     FileNotFoundError = IOError
 
 def test_send_message_with_invalid_credentials():
-	with pytest.raises(FileNotFoundError) as e_info:
+	with pytest.raises(FileNotFoundError):
 		message = None
 		with open("C:\\DjangoCourse\\Courses\\celery\\introduction-promo.mp4", "rb") as image_file:
 			encoded_string = base64.b64encode(image_file.read())
 			message = encoded_string.decode("utf-8")
 
-		sqs = SQSClientExtended(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION, 'tiptapcode-sqs-data')
+		sqs = SQSClientExtended(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION, 
+		'tiptapcode-sqs-data')
 
 		# _100mb_large_string = ''.join([choice(ascii_letters + digits) for i in range(104857600)])
 
